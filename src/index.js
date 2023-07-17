7feeeffe7ad39dc4a73102e5cae7e19ed3dd64fa
Muhammad Arslan <muhammad.arslan@iodigital.com>
2023-07-17 17:31:50 +0200

    Code Update

--- src/index.js
+++ src/index.js
@@ -1,58 +1,49 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+//const { MongoClient, ServerApiVersion } = require("mongodb");
+import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
+dotenv.config();
 
-const books = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    title: "City of Glass",
-    author: "Paul Auster",
-  },
-];
+const { DB_URI, DB_NAME } = process.env;
 
 const typeDefs = `#graphql
-
-    # The "Query" type is special: it lists all of the available queries that
-    # clients can execute, along with the return type for each. In this
-    # case, the "books" query returns an array of zero or more Books (defined above).
-    type Query {
-        books: [Book]
-    }
-
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
+  type User {
+    id: ID!
   }
-
- 
 `;
 
-const resolvers = {
-  Query: {
-    books: () => books,
+const resolvers = {};
+
+const client = new MongoClient(DB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
   },
+});
+const start = async () => {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const db = client.db(DB_NAME);
+
+    const context = { db };
+
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+      context,
+    });
+    const { url } = await startStandaloneServer(server, {
+      listen: { port: 4000 },
+    });
+
+    console.log(`🚀  Server ready at: ${url}`);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 };
-
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
-
-// Passing an ApolloServer instance to the `startStandaloneServer` function:
-//  1. creates an Express app
-//  2. installs your ApolloServer instance as middleware
-//  3. prepares your app to handle incoming requests
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
-});
-
-console.log(`🚀  Server ready at: ${url}`);
+start().catch(console.dir);
